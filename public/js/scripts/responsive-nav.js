551b1717b6bca22c305c9ff9005346f655206a3d
Kirsty Rose <krhome21@gmail.com>
2016-08-11 16:10:18 +0100

    bugfix: fix z-indexing of donate button on navigation

--- public/js/scripts/responsive-nav.js
+++ public/js/scripts/responsive-nav.js
@@ -2,11 +2,17 @@ $(function() {
   $('.nav-toggle').on('click', function(){
     $('.navigation').slideToggle();
   });
-});
-
-$(function(){
-    $('nav a').click(function(){
-      $('a').removeClass("active");
-      $(this).addClass("active");
+  var clicked = false;
+  $('.nav-toggle').click(function () {
+      clicked = true;
   });
+  if (clicked) {
+    $('.donate-anchor').css({
+      'z-index': -1
+    });
+  } else {
+    $('.donate-anchor').css({
+      'z-index': 1
+    });
+  }
 });
